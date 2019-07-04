7a09636adf7facb7afe399db6be7c7ab8b540d3d
liutong <jackie6053@outlook.com>
2019-07-04 11:36:54 +0800

    init project

---
+++ src/main/java/com/ltpc/demo/springcloudgwd/SpringCloudGwDApplication.java
@@ -0,0 +1,13 @@
+package com.ltpc.demo.springcloudgwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGwDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGwDApplication.class, args);
+    }
+
+}
